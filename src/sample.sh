be8ad4900230b13a4738a2cbeb4863ab6c81fb24
root <root@ip-10-12-0-9.ap-northeast-1.compute.internal>
2020-03-25 09:18:21 +0000

    add sh

--- src/sample.sh
+++ src/sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/lib/jenkins/workspace/gitlab-integration-test/src/
 pwd
-echo "Main-Class: Sample" >> manifest.mf
 javac hello.java
+echo "Main-Class: Sample" > manifest.mf
 jar -cvfm hello.jar manifest.mf *.class
 java -jar hello.jar
