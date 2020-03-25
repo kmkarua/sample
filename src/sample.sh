f7aff3bad1e2772562f87983ff3f402f982688d0
root <root@ip-10-12-0-9.ap-northeast-1.compute.internal>
2020-03-25 08:37:40 +0000

    add script

--- src/sample.sh
+++ src/sample.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/lib/jenkins/workspace/gitlab-integration-test/src/
-echo "Main-Class: Sample" >> manifest.mf
+echo "Main-Class: Sample" >> /var/lib/jenkins/workspace/gitlab-integration-test/src/manifest.mf
 jar -cvfm Sample.jar manifest.mf *.class
 java -jar Sample.jar
